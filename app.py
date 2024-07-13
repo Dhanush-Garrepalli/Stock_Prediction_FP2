83d563e15f0794cd785afa580f79ef3e0013bba9
Garrepalli Dhanush <35004425+Dhanush-Garrepalli@users.noreply.github.com>
2024-07-13 21:15:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import streamlit as st
  
 # Initialize a session using Amazon Forecast
 session = boto3.Session(
-    aws_access_key_id='AKIA47CRXULRB2H562MZ',
-    aws_secret_access_key='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',
+    aws_access_key_id='AKIAU6GDV7LVBUZHSS5H',
+    aws_secret_access_key='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',
     region_name='ap-south-1'
 )
  
@@ -43,14 +43,14 @@ def get_forecast(forecast_arn, item_id):
 # Streamlit UI
 st.title("Forecast Dashboard")
  
-item_id = st.text_input("Enter Stock Name", "Britannia")
+item_id = st.text_input("Enter Stock Name", "DHANUKA")
 forecast_type = st.selectbox("Select Forecast Type", ["Short Term", "Long Term"])
  
 if st.button("Get Forecast"):
     if forecast_type == "Short Term":
-        forecast_arn = 'arn:aws:forecast:ap-south-1:891377132258:forecast/FP2Forecast'
+        forecast_arn = 'arn:aws:forecast:ap-south-1:339712801514:forecast/Group16_Forecast'
     else:
-        forecast_arn = 'arn:aws:forecast:ap-south-1:891377132258:forecast/FP2Forecast2'
+        forecast_arn = 'arn:aws:forecast:ap-south-1:339712801514:forecast/Group16_Forecast'
     df_forecast = get_forecast(forecast_arn, item_id)
     if not df_forecast.empty:
         st.write(f"{forecast_type} Forecast for {item_id}")
